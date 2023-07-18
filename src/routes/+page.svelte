a3bd3c42a11bbb572ff4adc636880c712461f834
John Wilson (1000118161) <WILSJW2@student.op.ac.nz>
2023-07-19 09:28:51 +1200

    Created cool website HTML with links and to-do list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,20 @@
-<h1>John Wilson Epic Website</h1>
+<h1>John Wilson Best Cool Website</h1>
+<h2>only the best are welcome</h2>
+
+<p>you are not</p>
+
+<br>
+<hr>
+
+<h3>To-do:</h3>
+<ol>
+    <li>become cool</li>
+    <li>get money</li>
+    <li>swagger</li>
+</ol>
+
+<br>
+<hr>
+
+<h2>Welcome back</h2>
+<p><a href="https://www.youtube.com/@ashures_" target="_blank"><img alt="YT icon" src="images/youtube.png" width="24">@Ashures on YouTube</a></p>
