75e583fb280a9f04ad10165ba556b4c7bc3520e4
John Wilson (1000118161) <WILSJW2@student.op.ac.nz>
2023-07-28 11:08:56 +1200

    Added a background image to index

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,21 @@
     export let data;
 </script>
 
-<style lang="js">
+<style>
+    .background {
+        height: 100%;
+        width: 100.1%;
+        transform: translate(-0.5%, -0.8%);
+        background-image: url("images/background.jpg");
+        background-repeat: no-repeat;
+        background-size: contain;
+        position: absolute;
+    }
+
+    .main {
+        padding: 0 1rem;
+    }
+
     h1 {
         color: maroon;
     }
@@ -39,50 +53,55 @@
     }
 </style>
 
-<h1>John Wilson Best Cool Website</h1>
-<h2>only the best are welcome</h2>
+<div class="background">
+    <div class="main">
+        <h1>John Wilson Best Cool Website</h1>
+        <h2>only the best are welcome</h2>
 
-<p>you are not</p>
+        <p>you are not</p>
 
-<br>
-<hr>
+        <br>
+        <hr>
 
-<h3>To-do:</h3>
-<ol>
-    <li>become cool</li>
-    <li>get money</li>
-    <li>swagger</li>
-</ol>
+        <h3>To-do:</h3>
+        <ol>
+            <li>become cool</li>
+            <li>get money</li>
+            <li>swagger</li>
+        </ol>
 
-<br>
-<hr>
+        <br>
+        <hr>
 
-<h2>Welcome</h2>
-<img id="cyberpunk" alt="Favourite game right now" src="images/cyberpunk.jpg">
+        <h2>Welcome</h2>
+        <img id="cyberpunk" alt="Favourite game right now" src="images/cyberpunk.jpg">
 
-<ul class="socials">
-    <li id="yt">
-        <img id="yt-icon" alt="YT icon" src="images/youtube.png">
-        <a href="https://www.youtube.com/@ashures_" target="_blank">
-            <p>YouTube</p>
-        </a>
-    </li>
-    <li id="twitter">
-        <img id="yt-icon" alt="YT icon" src="images/youtube.png">
-        <a href="https://www.twitter.com/ashures_" target="_blank">
-            <p>Twitter</p>
-        </a>
-    </li>
-    <li id="twitch">
-        <img id="yt-icon" alt="YT icon" src="images/youtube.png">
-        <a href="https://www.twitch.com/ashuress" target="_blank">
-            <p>Twitch</p>
-        </a>
-    </li>
-    <li id="games">
-        <img id="yt-icon" alt="YT icon" src="images/youtube.png">
-        <a href="https://ashures.w3spaces.com/" target="_blank">
-            <p>Games</p>
-        </a>
-    </li>
-</ul>
+        <ul class="socials">
+            <li id="yt">
+                <img id="yt-icon" alt="YT icon" src="images/youtube.png">
+                <a href="https://www.youtube.com/@ashures_" target="_blank">
+                    <p>YouTube</p>
+                </a>
+            </li>
+            <li id="twitter">
+                <img id="yt-icon" alt="YT icon" src="images/youtube.png">
+                <a href="https://www.twitter.com/ashures_" target="_blank">
+                    <p>Twitter</p>
+                </a>
+            </li>
+            <li id="twitch">
+                <img id="yt-icon" alt="YT icon" src="images/youtube.png">
+                <a href="https://www.twitch.com/ashuress" target="_blank">
+                    <p>Twitch</p>
+                </a>
+            </li>
+            <li id="games">
+                <img id="yt-icon" alt="YT icon" src="images/youtube.png">
+                <a href="https://ashures.w3spaces.com/" target="_blank">
+                    <p>Games</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+
