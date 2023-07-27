89861598ae881f67be8112ef30af6bd209fe98b9
Ashures <ashuresyt@gmail.com>
2023-07-27 18:15:29 +1200

    adding styles to the socials links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,15 +5,32 @@
 
     img#cyberpunk {
         width: 480px;
+        transition: ease-out width 0.1s;
     }
 
     img#cyberpunk:hover {
         width: 720px;
-        transition: ease-in width 0.2s;
+        transition: ease-in width 0.1s;
     }
 
     img#cyberpunk {
         width: 480px;
+    }
+
+    .socials * {
+        margin: 0 0 0.2rem 0.4rem;
+        text-decoration: none;
+        display: flex;
+    }
+
+    .socials a:visited {
+        margin: 0;
+        text-decoration: none;
+    }
+
+    .socials img {
+        width: 36px;
+        margin: -0.6rem 0 0 -2rem;
     }
 </style>
 
@@ -37,4 +54,18 @@
 
 <h2>Welcome</h2>
 <img id="cyberpunk" alt="Favourite game right now" src="images/cyberpunk.jpg">
-<p><a href="https://www.youtube.com/@ashures_" target="_blank"><img alt="YT icon" src="images/youtube.png" width="24">@Ashures on YouTube</a></p>
+
+<ul class="socials">
+    <li id="yt">
+        <img id="yt-icon" alt="YT icon" src="images/youtube.png">
+        <a href="https://www.youtube.com/@ashures_" target="_blank">
+            <p>YouTube</p>
+        </a>
+    </li>
+    <li id="twitter">
+        <img id="yt-icon" alt="YT icon" src="images/youtube.png">
+        <a href="https://www.twitter.com/ashures_" target="_blank">
+            <p>Twitter</p>
+        </a>
+    </li>
+</ul>
