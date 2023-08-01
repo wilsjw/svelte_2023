16591deb36a1eae9c5867f1ac446fed674c8d1ad
wilsjw2 <wilsjw2@student.op.ac.nz>
2023-08-02 09:48:56 +1200

    Added home page link

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -20,8 +20,8 @@
 
 <nav>
     <div id="links">
-        <a href="about">About Me</a>
-        <a href="lying">Page 2</a>
+        <a href="/">Home</a>
+        <a href="/about">About Me</a>
         <a href="liar">Page 3</a>
         <a href="lied">Page 4</a>
         <a href="lies">Page 5</a>
