b169d4cfe21a4d3670520d3c90ad2df0736fe6b3
wilsjw2 <wilsjw2@student.op.ac.nz>
2023-08-02 08:30:51 +1200

    Created a nav bar component

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,22 @@
+<style>
+    nav {
+        width: 100%;
+        background-color: pink;
+        padding: 2rem 4rem;
+    }
+
+    nav a {
+        padding: 0 4rem;
+        display: inline-block;
+        text-decoration: none;
+        color: black;
+    }
+</style>
+
+<nav>
+    <a href="ashures.w3spaces.com">Page 1</a>
+    <a href="ashures.w3spaces.com">Page 2</a>
+    <a href="ashures.w3spaces.com">Page 3</a>
+    <a href="ashures.w3spaces.com">Page 4</a>
+    <a href="ashures.w3spaces.com">Page 5</a>
+</nav>
