5c67940a4549ed5520dac981b931b0a5563f2de3
wilsjw2 <wilsjw2@student.op.ac.nz>
2023-08-02 09:27:30 +1200

    Centred nav links

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -5,18 +5,25 @@
         padding: 2rem 4rem;
     }
 
+    nav #links {
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+    }
+
     nav a {
         padding: 0 4rem;
-        display: inline-block;
         text-decoration: none;
         color: black;
     }
 </style>
 
 <nav>
-    <a href="about">About Me</a>
-    <a href="lying">Page 2</a>
-    <a href="liar">Page 3</a>
-    <a href="lied">Page 4</a>
-    <a href="lies">Page 5</a>
+    <div id="links">
+        <a href="about">About Me</a>
+        <a href="lying">Page 2</a>
+        <a href="liar">Page 3</a>
+        <a href="lied">Page 4</a>
+        <a href="lies">Page 5</a>
+    </div>
 </nav>
