8d6f9ed8afad8c1ba84e39f161435dff7246d29f
wilsjw2 <wilsjw2@student.op.ac.nz>
2023-08-04 10:12:50 +1200

    Changed the icon link to the new icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,19 +43,19 @@
                     </a>
                 </li>
                 <li id="twitter">
-                    <img id="yt-icon" alt="YT icon" src="images/youtube.png">
+                    <img id="twitter-icon" alt="Twitter icon" src="images/twitter.png">
                     <a href="https://www.twitter.com/ashures_" target="_blank">
                         <p>Twitter</p>
                     </a>
                 </li>
                 <li id="twitch">
-                    <img id="yt-icon" alt="YT icon" src="images/youtube.png">
+                    <img id="twitch-icon" alt="YT icon" src="images/twitch.png">
                     <a href="https://www.twitch.com/ashuress" target="_blank">
                         <p>Twitch</p>
                     </a>
                 </li>
                 <li id="games">
-                    <img id="yt-icon" alt="YT icon" src="images/youtube.png">
+                    <img id="games-icon" alt="YT icon" src="images/games.png">
                     <a href="https://ashures.w3spaces.com/" target="_blank">
                         <p>Games</p>
                     </a>
