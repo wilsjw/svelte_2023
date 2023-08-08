73a75a9b3a210d239bb874b26b5a8f8b85660604
wilsjw2 <wilsjw2@student.op.ac.nz>
2023-08-09 08:38:06 +1200

    Moved the social links to the footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -42,31 +42,6 @@
     </div>
     
     <div id="bottom">
-        <ul class="socials">
-            <li id="yt">
-                <img id="yt-icon" alt="YT icon" src="images/youtube.png">
-                <a href="https://www.youtube.com/@ashures_" target="_blank">
-                    <p>YouTube</p>
-                </a>
-            </li>
-            <li id="twitter">
-                <img id="twitter-icon" alt="Twitter icon" src="images/twitter.png">
-                <a href="https://www.twitter.com/ashures_" target="_blank">
-                    <p>Twitter</p>
-                </a>
-            </li>
-            <li id="twitch">
-                <img id="twitch-icon" alt="YT icon" src="images/twitch.png">
-                <a href="https://www.twitch.com/ashuress" target="_blank">
-                    <p>Twitch</p>
-                </a>
-            </li>
-            <li id="games">
-                <img id="games-icon" alt="YT icon" src="images/games.png">
-                <a href="https://ashures.w3spaces.com/" target="_blank">
-                    <p>Games</p>
-                </a>
-            </li>
-        </ul>
+        
     </div>
 </div>
