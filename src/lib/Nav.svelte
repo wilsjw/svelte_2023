9e0380bace710dbbfb8ff353bddaa2ce569573d9
Ashures <ashuresyt@gmail.com>
2023-08-13 14:17:40 +1200

    Added fancy underline to navbar and social links

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,9 +1,9 @@
 <nav>
     <div id="links">
-        <a href="/">Home</a>
-        <a href="/about">About Me</a>
-        <a href="liar">Page 3</a>
-        <a href="lied">Page 4</a>
-        <a href="lies">Page 5</a>
+        <a href="/" class="fancy-underline">Home</a>
+        <a href="/about" class="fancy-underline">About Me</a>
+        <a href="liar" class="fancy-underline">Page 3</a>
+        <a href="lied" class="fancy-underline">Page 4</a>
+        <a href="lies" class="fancy-underline">Page 5</a>
     </div>
 </nav>
