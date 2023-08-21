58d5dda2caf62afa11cbe9b4d1e75fcf00efe924
Ashures <ashuresyt@gmail.com>
2023-08-21 22:17:36 +1200

    Extracted pokemon list to component

---
+++ src/lib/PokemonList.svelte
@@ -0,0 +1,26 @@
+<div class="pokemon-grid">
+    <img on:click={event => imageClick(event)} src="images/home/abra.jpg" alt="abra" id="abra" class="clickable">
+    <img on:click={event => imageClick(event)} src="images/home/bulbasaur.jpg" alt="bulbasaur" id="bulbasaur" class="clickable">
+    <img on:click={event => imageClick(event)} src="images/home/caterpie.jpg" alt="caterpie" id="caterpie" class="clickable">
+    <img on:click={event => imageClick(event)} src="images/home/charmander.jpg" alt="charmander" id="charmander" class="clickable">
+    
+    <img on:click={event => imageClick(event)} src="images/home/cubone.jpg" alt="cubone" id="cubone" class="clickable">
+    <img on:click={event => imageClick(event)} src="images/home/dratini.jpg" alt="dratini" id="dratini" class="clickable">
+    <img on:click={event => imageClick(event)} src="images/home/eevee.jpg" alt="eevee" id="eevee" class="clickable">
+    <img on:click={event => imageClick(event)} src="images/home/geodude.jpg" alt="geodude" id="geodude" class="clickable">
+    
+    <img on:click={event => imageClick(event)} src="images/home/grimer.jpg" alt="grimer" id="grimer" class="clickable">
+    <img on:click={event => imageClick(event)} src="images/home/jigglypuff.jpg" alt="jigglypuff" id="jigglypuff" class="clickable">
+    <img on:click={event => imageClick(event)} src="images/home/machop.jpg" alt="machop" id="machop" class="clickable">
+    <img on:click={event => imageClick(event)} src="images/home/magnemite.jpg" alt="magnemite" id="magnemite" class="clickable">
+    
+    <img on:click={event => imageClick(event)} src="images/home/mew.jpg" alt="mew" id="mew" class="clickable">
+    <img on:click={event => imageClick(event)} src="images/home/paras.jpg" alt="paras" id="paras" class="clickable">
+    <img on:click={event => imageClick(event)} src="images/home/pidgey.jpg" alt="pidgey" id="pidgey" class="clickable">
+    <img on:click={event => imageClick(event)} src="images/home/pikachu.jpg" alt="pikachu" id="pikachu" class="clickable">
+    
+    <img on:click={event => imageClick(event)} src="images/home/psyduck.jpg" alt="psyduck" id="psyduck" class="clickable">
+    <img on:click={event => imageClick(event)} src="images/home/rattata.jpg" alt="rattata" id="rattata" class="clickable">
+    <img on:click={event => imageClick(event)} src="images/home/sandshrew.jpg" alt="sandshrew" id="sandshrew" class="clickable">
+    <img on:click={event => imageClick(event)} src="images/home/squirtle.jpg" alt="squirtle" id="squirtle" class="clickable">
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+    import PokemonList from "../lib/PokemonList.svelte";
+
     let focusImage = "images/home/mew.jpg";
     let focusTitle = "mew";
 
@@ -38,32 +40,7 @@
         </div>
 
         <h2>All Pokémon. Ever.</h2>
-        <div class="pokemon-grid">
-            <img on:click={event => imageClick(event)} src="images/home/abra.jpg" alt="abra" id="abra" class="clickable">
-            <img on:click={event => imageClick(event)} src="images/home/bulbasaur.jpg" alt="bulbasaur" id="bulbasaur" class="clickable">
-            <img on:click={event => imageClick(event)} src="images/home/caterpie.jpg" alt="caterpie" id="caterpie" class="clickable">
-            <img on:click={event => imageClick(event)} src="images/home/charmander.jpg" alt="charmander" id="charmander" class="clickable">
-            
-            <img on:click={event => imageClick(event)} src="images/home/cubone.jpg" alt="cubone" id="cubone" class="clickable">
-            <img on:click={event => imageClick(event)} src="images/home/dratini.jpg" alt="dratini" id="dratini" class="clickable">
-            <img on:click={event => imageClick(event)} src="images/home/eevee.jpg" alt="eevee" id="eevee" class="clickable">
-            <img on:click={event => imageClick(event)} src="images/home/geodude.jpg" alt="geodude" id="geodude" class="clickable">
-            
-            <img on:click={event => imageClick(event)} src="images/home/grimer.jpg" alt="grimer" id="grimer" class="clickable">
-            <img on:click={event => imageClick(event)} src="images/home/jigglypuff.jpg" alt="jigglypuff" id="jigglypuff" class="clickable">
-            <img on:click={event => imageClick(event)} src="images/home/machop.jpg" alt="machop" id="machop" class="clickable">
-            <img on:click={event => imageClick(event)} src="images/home/magnemite.jpg" alt="magnemite" id="magnemite" class="clickable">
-            
-            <img on:click={event => imageClick(event)} src="images/home/mew.jpg" alt="mew" id="mew" class="clickable">
-            <img on:click={event => imageClick(event)} src="images/home/paras.jpg" alt="paras" id="paras" class="clickable">
-            <img on:click={event => imageClick(event)} src="images/home/pidgey.jpg" alt="pidgey" id="pidgey" class="clickable">
-            <img on:click={event => imageClick(event)} src="images/home/pikachu.jpg" alt="pikachu" id="pikachu" class="clickable">
-            
-            <img on:click={event => imageClick(event)} src="images/home/psyduck.jpg" alt="psyduck" id="psyduck" class="clickable">
-            <img on:click={event => imageClick(event)} src="images/home/rattata.jpg" alt="rattata" id="rattata" class="clickable">
-            <img on:click={event => imageClick(event)} src="images/home/sandshrew.jpg" alt="sandshrew" id="sandshrew" class="clickable">
-            <img on:click={event => imageClick(event)} src="images/home/squirtle.jpg" alt="squirtle" id="squirtle" class="clickable">
-        </div>
+        <PokemonList />
     </div>
     
     <div id="bottom">
