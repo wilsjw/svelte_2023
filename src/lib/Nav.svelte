d01d64bf182ae67d4628dd3f5fa7aa689ff2171e
wilsjw <wilsjw2@student.op.ac.nz>
2023-09-01 11:02:35 +1200

    Replaced navbar text with icons

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,9 +1,5 @@
 <nav>
     <div id="links">
-        <a href="/" class="fancy-underline">Home</a>
-        <a href="/about" class="fancy-underline">About Me</a>
-        <a href="liar" class="fancy-underline">Page 3</a>
-        <a href="lied" class="fancy-underline">Page 4</a>
-        <a href="lies" class="fancy-underline">Page 5</a>
+        <a href="/"><i class="fa-solid fa-house fa-2xl spin"></i></a>
     </div>
 </nav>
