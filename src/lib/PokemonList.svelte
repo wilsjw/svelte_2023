f2bdcd4bd8e62a50112ff6a4a79422330f10bd98
Ashures <ashuresyt@gmail.com>
2023-09-03 19:52:38 +1200

    Replaced old pokemon cards with PokemonCard

--- src/lib/PokemonList.svelte
+++ src/lib/PokemonList.svelte
@@ -1,4 +1,6 @@
 <script>
+    import PokemonCard from "./PokemonCard.svelte";
+
     export let pokemon;
 
     let spriteUrl = "https://img.pokemondb.net/artwork/large/"
@@ -6,13 +8,6 @@
 
 <div class="pokemon-grid">
     {#each pokemon as p}
-        <a href="/pokemon/{p}">
-            <div class="card">
-                <div class="card-image" style={"background-image: url('" + spriteUrl + p + ".jpg')"}></div>
-                <div class="card-info">
-                    <h2>{p}</h2>
-                </div>
-            </div>
-        </a>
+        <PokemonCard p={p} />
     {/each}
 </div>
