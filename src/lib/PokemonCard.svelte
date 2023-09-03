fe52cb791bb1b20fecd08ae76a6688a06b3c5310
Ashures <ashuresyt@gmail.com>
2023-09-03 19:52:12 +1200

    Moved the pokemon cards into PokemonCard

---
+++ src/lib/PokemonCard.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let p;
+</script>
+
+<a href="/pokemon/{p}">
+    <div class="card">
+        <div class="card-image" style="background-image: url('https://img.pokemondb.net/artwork/large/{p}.jpg')"></div>
+        <div class="card-info">
+            <h2>{p}</h2>
+        </div>
+    </div>
+</a>
