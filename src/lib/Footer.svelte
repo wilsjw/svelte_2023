c4bce8c53c31c83c9509a3f4c9891e853728dc62
wilsjw <wilsjw2@student.op.ac.nz>
2023-10-26 10:36:24 +1300

    Renamed SocialLinks to SocialLink

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,12 +1,12 @@
 <script>
-  import SocialLinks from "./SocialLinks.svelte";
+  import SocialLink from "./SocialLink.svelte";
 
 </script>
 <footer>
     <ul class="socials">
-        <SocialLinks title="YouTube" link="https://www.youtube.com/@ashures_" img="/images/youtube.png" />
-        <SocialLinks title="Twitter" link="https://www.twitter.com/ashures_" img="/images/twitter.png" />
-        <SocialLinks title="Twitch" link="https://www.twitch.com/ashuress" img="/images/twitch.png" />
-        <SocialLinks title="Games" link="https://ashures.w3spaces.com/" img="/images/games.png" />
+        <SocialLink title="YouTube" link="https://www.youtube.com/@ashures_" img="/images/youtube.png" />
+        <SocialLink title="Twitter" link="https://www.twitter.com/ashures_" img="/images/twitter.png" />
+        <SocialLink title="Twitch" link="https://www.twitch.com/ashuress" img="/images/twitch.png" />
+        <SocialLink title="Games" link="https://ashures.w3spaces.com/" img="/images/games.png" />
     </ul>
 </footer>
