c41a1887cfc218e900d4cf4ec4037ee30cd9e365
wilsjw <wilsjw2@student.op.ac.nz>
2023-10-26 10:34:56 +1300

    Added html using attributes to SocialLinks

--- src/lib/SocialLinks.svelte
+++ src/lib/SocialLinks.svelte
@@ -1,7 +1,12 @@
 <script>
     export let title;
-    export let alt;
     export let link;
     export let img;
 </script>
 
+<li id={title.toLowerCase()}>
+    <img id={`${title.toLowerCase()}-icon`} alt={`${title} icon`} src={img}>
+    <a href={link} target="_blank" class="fancy-underline">
+        <p>{title}</p>
+    </a>
+</li>
